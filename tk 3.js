d0aabb0f66db1454a20b9b8a2ff289c3e5eec587
awarepalak10 <awarepalak@gmail.com>
2026-01-15 21:49:13 +0530

    trigger

--- tk 3.js
+++ tk 3.js
@@ -4,6 +4,20 @@ let email = document.querySelector('.email');
 
 let name = document.querySelector('.name');
 
+let name = document.getElementById("showname").addEventListener("change",function())
+{
+    const passwordinput=document.getElementById("name");
+    if(this.check){
+        //show name
+        passwordinput.type="text",
+
+    }
+    else{
+       //hide name
+       passwordinput.type="name";
+    }
+});
+
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
