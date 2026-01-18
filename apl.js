796f08e950ff774360b333b56f76232e37f8c0af
awarepalak10 <awarepalak@gmail.com>
2026-01-18 10:23:58 +0530

    APL.HTML

---
+++ apl.js
@@ -0,0 +1,33 @@
+
+
+fetch("https://jsonplaceholder.typicode,com/users")
+.then( response => response.json())
+.then( data => {
+
+    console.log(data);
+})
+.catch(error=> {
+    console.log("error is as follows : ", error)
+
+})
+
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then( response => response.json())
+.then( user => {
+    let list = document.querySelector(".list");
+
+    user.forEach( user => {
+        let li = document.createElement('li')
+        li.textContent = user.name;
+        list.appendChild(li)
+    }
+)
+}
+)
+.catch(error => {
+    console.log("error is as follows : ", error)
+}
+
+)
